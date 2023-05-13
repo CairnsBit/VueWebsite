ff584128bfe110a090ea385a7cec4121f33d87bf
CairnsBit <mnowicki586@outlook.com>
2023-05-13 12:20:46 +0200

    logo icon fix

--- VueComponents/App.vue
+++ VueComponents/App.vue
@@ -5,7 +5,7 @@ import TheWelcome from '../VueComponents/TheWelcome.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./Style/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="../Style/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
